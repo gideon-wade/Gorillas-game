4adfebf45ea84833e66e2bd508d42b260883b8e8
soender2 <rasm847i@gmail.com>
2022-01-03 14:00:39 +0100

    rettelse på PLayer-class

--- src/main/java/ApplicationClasses/Player.java
+++ src/main/java/ApplicationClasses/Player.java
@@ -7,8 +7,8 @@ public class Player {
         starting_position[0] = position;
     }
 
-    public void throwing(int velocity, int gravity, int angle) {
+    public void throwing(int velocity, double gravity, int angle) {
         Banana banan = new Banana(velocity, gravity, angle);
-        banan.trajectory(velocity, angle);
+        banan.trajectory();
     }
 }
