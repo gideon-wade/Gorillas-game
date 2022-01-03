83c139703f77696058a15ba4107f3f8a29c2f12f
Gideon <gidewadejob@gmail.com>
2022-01-03 15:14:12 +0100

    ui updated

--- src/main/java/ApplicationClasses/Player.java
+++ src/main/java/ApplicationClasses/Player.java
@@ -3,6 +3,7 @@ package ApplicationClasses;
 public class Player {
     private int[] starting_position = new int[2];
     private String name;
+    private Boolean theirTurn;
 
     public Player(int positionX, int positionY, String name) {
         starting_position[0] = positionX;
