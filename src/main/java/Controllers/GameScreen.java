c4dd9030db5d305b8014151345d3dd10726820c5
OscarWulff <s216163@dtu.dk>
2022-01-05 14:55:13 +0100

    ingen enter

--- src/main/java/Controllers/GameScreen.java
+++ src/main/java/Controllers/GameScreen.java
@@ -74,8 +74,12 @@ public class GameScreen {
     }
 
     public void doThrow(ActionEvent event) throws IOException {
-        Thread thread = new Thread(this::runThread);
-        thread.start();
+        this.playerOneAngle = Integer.parseInt(pl1ang.getText());
+        this.playerOneVelocity = Integer.parseInt(pl1vec.getText());
+            Thread thread = new Thread(this::runThread);
+            thread.start();
+                this.playerTwoAngle = Integer.parseInt(pl2ang.getText());
+                this.playerTwoVelocity = Integer.parseInt(pl2vec.getText());
 
     }
 
@@ -170,9 +174,5 @@ public class GameScreen {
         return this.list;
     }
 }
-
-
-
-
 
 
