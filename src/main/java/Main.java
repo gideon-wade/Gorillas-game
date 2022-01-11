803699a914810a912e07ee806c60e6f20a6ec850
OscarWulff <s216163@dtu.dk>
2022-01-11 15:38:36 +0100

    rettelse

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,6 @@ import Controllers.SceneManager;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
-import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -15,7 +14,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws IOException {
-        Scene container = new Scene(new Pane(),1300,800,Color.RED);
+        Scene container = new Scene(new Pane(),1300,800);
         SceneManager.setMainScene(container);
         primaryStage.setScene(container);
         SceneManager.changeScene("fxml/MainScene.fxml");
